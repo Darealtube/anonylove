66e8b9f4f73740a0816913a0fd81e331713f7721
Darealtube <evajavier143@gmail.com>
2022-05-01 12:10:52 +0800

    Try This

--- pages/activeChat.tsx
+++ pages/activeChat.tsx
@@ -1,4 +1,4 @@
-import { useMutation, useQuery } from "@apollo/client";
+import { useMutation, useQuery, useSubscription } from "@apollo/client";
 import {
   AppBar,
   Box,
@@ -77,6 +77,13 @@ const ActiveChat = ({ name }: { name: string }) => {
       name,
     },
   });
+  const { data: newMessage } = useSubscription(GET_USER_ACTIVE_CHAT, {
+    variables: {
+      name,
+    },
+  });
+
+  console.log(newMessage)
   const confessedTo = session?.user?.name === getUserActiveChat?.confessee.name;
 
   const handleOpenEmoji = (e: React.MouseEvent<HTMLButtonElement>) => {

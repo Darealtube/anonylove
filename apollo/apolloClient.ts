c4400e55d28a0cdc0b35e741de0476e51132b42c
Darealtube <evajavier143@gmail.com>
2022-09-10 12:07:04 +0800

    FixLink

--- apollo/apolloClient.ts
+++ apollo/apolloClient.ts
@@ -32,7 +32,7 @@ const authLink = setContext(async (_, previousContext) => {
 });
 
 const httpLink = new HttpLink({
-  uri: "http://localhost:3000/graphql",
+  uri: "https://anony-api-3.herokuapp.com/graphql",
   credentials: "include",
   fetchOptions: {
     mode: "cors", // This should always be CORS as our API server is on another domain.
@@ -43,7 +43,7 @@ const wsLink =
   typeof window !== "undefined"
     ? new GraphQLWsLink(
         createClient({
-          url: "ws://localhost:3000/graphql",
+          url: "wss://anony-api-3.herokuapp.com/graphql",
           connectionParams: async () => {
             const session = await getSession();
             return { Authorization: session?.user.id };
